6367a49d7ed705011658a7d056091c5981fc1c52
Mr-andrei <91520213+Mr-andrei@users.noreply.github.com>
2022-01-12 16:33:12 +0300

    next commit 12 01

--- myJs.js
+++ myJs.js
@@ -120,7 +120,6 @@
 //
 
 
-
 // function digitize(n) {
 //   let newArr = String(n).split("").reverse().join()
 //   let b =[]
@@ -174,41 +173,94 @@
 // console.log(getAverage([1,2,3,4,5,]))
 //
 
+//
+// function countPositivesSumNegatives(input) {
+//   let polsum = 0
+//   let minusSum = 0
+//   if(input.length <= 0  ){
+//     return []
+//   }
+//   else{
+//   for(let i = 0; i < input.length; i++){
+//     if(input[i] !== 0 && input[i] > 0 ){
+//       polsum += 1
+//     }
+//     else if( input[i] !== 0 && input[i] < 0 ){
+//       minusSum += input[i]
+//     }
+//
+//   }
+//   return [polsum, minusSum]}
+// }
+//
+// console.log(countPositivesSumNegatives([]))
+//
+//
+// function paul(x) {
+//     let value = 0
+//     for(let i = 0; i < x.length; i++){
+//         if(x[i] === "kata"){
+//             value += 5
+//         }else if(x[i] === "Petes kata" ){
+//             value += 10
+//         }
+//         else if(x[i] === "life"){
+//             value += 0
+//         }
+//         else if(x[i] === "eating"){
+//             value += 1
+//         }
+//     }
+//    if(value < 40){
+//        return 'Super happy!'
+//    }
+//    else if( value >= 40 && value < 70){
+//        return 'Happy!'
+//    }
+//    else if(value >= 70 && value < 100){
+//        return 'Sad!'
+//    }
+//    else if (value > 100) return "Miserable!"
+//
+//
+// }
+//
+// console.log(paul(['life', 'Petes kata', 'Petes kata', 'Petes kata', 'eating']))
+//
 
-function countPositivesSumNegatives(input) {
-  let polsum = 0
-  let minusSum = 0
-  if(input.length <= 0  ){
-    return []
-  }
-  else{
-  for(let i = 0; i < input.length; i++){
-    if(input[i] !== 0 && input[i] > 0 ){
-      polsum += 1
-    }
-    else if( input[i] !== 0 && input[i] < 0 ){
-      minusSum += input[i]
-    }
-
-  }
-  return [polsum, minusSum]}
-}
-
-console.log(countPositivesSumNegatives([]))
+// kata = 5
+// Petes kata = 10
+// life = 0
+// eating = 1
+// < 40 = 'Super happy!'
+// < 70 >= 40 = 'Happy!'
+// < 100 >= 70 = 'Sad!'
+// > 100 = 'Miserable!'
 
 
+// function pairZeros(arr) {
+//     let newArr = []
+//    for(let i = 0; i < arr.length; i++){
+//        if(arr[i] !== 0){
+//            newArr.push(arr[i])
+//        }
+//        else if( arr[i] === 0){
+//        }
+//    }
+//    return newArr
+// }
+//
+// console.log(pairZeros([1,0,1,0,2,0,0,3,0]))    //[1,0,1,2,0,3,0]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// function getMaxValue (arr) {
+//     let max = arr[0]
+//     for (let i = 0; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+//
+// console.log(getMaxValue([1222,22,12,123,11,1111]))
